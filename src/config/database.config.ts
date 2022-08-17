530e1456d97b24fe285b280f18a5517713ffda75
muhireheir <muhire416@gmail.com>
2022-08-17 23:50:08 +0200

    deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,6 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 export default db;
